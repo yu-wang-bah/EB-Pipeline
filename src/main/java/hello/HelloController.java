93c6a46abcf13a72e9dbcce0aa3470e826f6eb49
Kevin Wang <yu.wang.bah@gmail.com>
2016-09-08 15:10:21 -0400

    back to where it was

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot Kevin3!";
+        return "Greetings from Spring Boot!";
     }
     
 }
