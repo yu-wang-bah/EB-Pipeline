f2765d931abb5828421aec820f3798186143a486
yu-wang-bah <yu.wang.bah@gmail.com>
2016-09-12 13:17:56 -0400

    added john 3

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot John2!";
+        return "Greetings from Spring Boot John3!";
     }
     
 }
