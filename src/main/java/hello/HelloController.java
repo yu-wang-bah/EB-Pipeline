1582715d9c17cfd3812d4c32617313fc5bb9af81
yu-wang-bah <yu.wang.bah@gmail.com>
2016-09-13 12:00:35 -0400

    KTP-1 fixed text

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot Kevin22!";
+        return "Greetings from Spring Boot Kevin!";
     }
 }
