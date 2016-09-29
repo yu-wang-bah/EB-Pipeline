a20be6434b8cfbf08fcd3b804254fa7f18934f37
Kevin Wang <yu.wang.bah@gmail.com>
2016-09-29 13:11:52 -0400

    KTP-5 demo text change

--- src/main/java/com/bah/web/controller/HelloController.java
+++ src/main/java/com/bah/web/controller/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class HelloController {
     
-	public static final String greeting = "Welcome to devops pipeline demo using cloud!";
+	public static final String greeting = "Welcome to devops pipeline demo using cloud! aws";
 	
     @RequestMapping("/")
     public String index() {
